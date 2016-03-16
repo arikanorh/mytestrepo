4ca07f7d8db90e308492fb02e468e4ac9f43a49e
Orhan ARIKAN <arikanorh@gmail.com>
2016-03-16 21:34:44 +0200

    Removes unused import

--- src/com/vebora/chat/server/data/DataSourceManager.java
+++ src/com/vebora/chat/server/data/DataSourceManager.java
@@ -2,23 +2,14 @@ package com.vebora.chat.server.data;
 
 import static com.googlecode.objectify.ObjectifyService.ofy;
 
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Key;
 
 public class DataSourceManager {
-
-	private DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
 
 	private DataSourceManager() {
 		// TODO Auto-generated constructor stub
 	}
 
-	/**
-	 * Lazy
-	 * 
-	 * @author MonsterPc
-	 */
 	private static class LazyInitializedInstanceHolder {
 		private static final DataSourceManager INSTANCE = new DataSourceManager();
 	}
